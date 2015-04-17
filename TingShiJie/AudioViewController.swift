d33ea23fb7329d996f5e2ad89204a6ba8d62b8ec
jiaji wang <jiaji.wang@rea-group.com>
2015-04-17 17:44:19 +1000

    add remote media controller

--- TingShiJie/AudioViewController.swift
+++ TingShiJie/AudioViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
 
 class AudioViewController: UIViewController {
 	
@@ -19,12 +20,27 @@ class AudioViewController: UIViewController {
 		super.viewDidLoad()
 
 		self.player = AVPlayer(URL: NSURL(string: channel.url!))
+		
+		if NSClassFromString("MPNowPlayingInfoCenter") != nil {
+			let image:UIImage = UIImage(named: self.channel.image!)!
+			let albumArt = MPMediaItemArtwork(image: image)
+			var songInfo: NSMutableDictionary = [
+				MPMediaItemPropertyTitle: self.channel.name!,
+				MPMediaItemPropertyArtwork: albumArt
+			]
+			MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = songInfo
+		}
+		
+		AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+		UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	
 	
 	@IBAction func buttonPressed(sender: AnyObject) {
 		toggle()
@@ -46,6 +62,18 @@ class AudioViewController: UIViewController {
 	func pauseRadio() {
 		player.pause()
 		playButton.setTitle("Play", forState: UIControlState.Normal)
+	}
+	
+	override func remoteControlReceivedWithEvent(event: UIEvent) {
+		if event.type == UIEventType.RemoteControl {
+			if event.subtype == UIEventSubtype.RemoteControlPlay {
+				playRadio()
+			} else if event.subtype == UIEventSubtype.RemoteControlPause {
+				pauseRadio()
+			} else if event.subtype == UIEventSubtype.RemoteControlTogglePlayPause {
+				toggle()
+			}
+		}
 	}
 	
 }
