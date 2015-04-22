52004e93d6d8408319a518fa5044aba2352b4868
jiaji wang <jiaji.wang@rea-group.com>
2015-04-22 18:12:51 +1000

    format audio player view

--- TingShiJie/AudioViewController.swift
+++ TingShiJie/AudioViewController.swift
@@ -15,8 +15,12 @@ class AudioViewController: UIViewController {
 	@IBOutlet weak var playButton: UIButton!
 	@IBOutlet weak var channelBrand: UIImageView!
 	@IBOutlet weak var channelInfo: UITextView!
+	
 	var player:  AVPlayer!
 	var channel: Channel!
+	
+	var playImage = UIImage(named: "play")
+	var pauseImage = UIImage(named: "pause")
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -24,10 +28,16 @@ class AudioViewController: UIViewController {
 		player = AVPlayer(URL: NSURL(string: channel.url!))
 		channelBrand.image = UIImage(named: channel.image!)
 		navigationItem.title = channel.name
+		
 		channelInfo.textAlignment = NSTextAlignment.Center
 		channelInfo.text = channel.info
 		channelInfo.backgroundColor = UIColor.clearColor()
 		channelInfo.textColor = UIColor.whiteColor()
+		
+		playButton.layer.borderWidth = 1
+		playButton.layer.borderColor = UIColor.whiteColor().CGColor
+		playButton.layer.cornerRadius = 5
+		
 		
 		if NSClassFromString("MPNowPlayingInfoCenter") != nil {
 			let image:UIImage = UIImage(named: channel.image!)!
